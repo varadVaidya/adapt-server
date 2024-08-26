9f6d15b9bbd93eacd0e436ada91298b9424b9fd9
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-08-27 05:01:08 +0530

    scale to big drone with better action buffer

--- adapt_drones/cfgs/environment_cfg.py
+++ adapt_drones/cfgs/environment_cfg.py
@@ -207,7 +207,7 @@ class TrajAviaryv2CTBRConfig:
 
         self.roll_pitch = [-0.15, 0.15] if not eval else [-0.15, 0.15]
 
-        self.scale_lengths = [0.05, 0.16] if self.scale else [0.05, 0.05]
+        self.scale_lengths = [0.05, 0.16] if self.scale else [0.16, 0.16]
 
         trajectory_path = pkg_resources.resource_filename(
             "adapt_drones", "assets/crazy_trajs_train.npy"
