ff2a3843da8cd1656c709401b12f5b15246d8601
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-09-08 11:22:08 +0530

    change the truncation and termination order to match the mujoco envs in gymnasium

--- adapt_drones/envs/TrajAviaryv2.py
+++ adapt_drones/envs/TrajAviaryv2.py
@@ -135,7 +135,7 @@ class TrajAviaryv2(BaseAviary):
         obs, reward, terminated, truncated, info = super().step(action)
         self.action_buffer = np.concatenate([self.action_buffer[1:], [action]])
 
-        return obs, reward, terminated, truncated, info
+        return obs, reward, truncated, False, info
 
     def _compute_obs(self):
         """
