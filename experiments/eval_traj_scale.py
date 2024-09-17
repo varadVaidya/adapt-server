11848039df01295ec6917c4100eae5e7357b74fd
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-09-17 11:32:32 +0530

    add idxed traj eval. still to test it

---
+++ experiments/eval_traj_scale.py
@@ -0,0 +1,130 @@
+import os
+import random
+import subprocess
+from dataclasses import dataclass
+import concurrent.futures
+from itertools import repeat
+
+os.environ["MUJOCO_GL"] = "egl"
+
+import numpy as np
+import torch
+import gymnasium as gym
+import tyro
+
+from adapt_drones.cfgs.config import *
+from adapt_drones.cfgs.environment_cfg import *
+from adapt_drones.utils.eval import paper_phase_1_eval, paper_RMA_DATT_eval
+from adapt_drones.utils.git_utils import check_git_clean
+
+# check_git_clean()  # TODO: see if needed in eval
+
+
+@dataclass
+class Args:
+    env_id: str
+    run_name: str
+    seed: int = 4551
+    agent: str = "RMA_DATT"
+    scale: bool = True
+
+
+env_runs = [["traj_v3", "different-jazz-16"]]
+
+
+def trajectory_eval_idx(idx, num_seeds, seeds, num_sc_list, sc_list, cfg):
+    print("Evalating trajectory idx:", idx)
+    # 8 : scale, seed, mean_e, rms_e,  mass, ixx, iyy, izz
+    phase_1_results = np.zeros((num_seeds, num_sc_list, 8))
+    rma_datt_results = np.zeros((num_seeds, num_sc_list, 8))
+
+    current_results = np.zeros(8)
+
+    for i, _seed in enumerate(seeds):
+        for j, _scale in enumerate(sc_list):
+            cfg.seed = _seed
+            cfg.environment.scale_lengths = _scale
+            cfg.scale.scale_lengths = _scale
+            # print("-----------==========++++++++++==========-----------")
+            results = paper_phase_1_eval(cfg=cfg, best_model=True, idx=idx)
+            current_results[0] = _scale[0]
+            current_results[1] = _seed
+            current_results[2:] = results
+
+            phase_1_results[i, j, :] = current_results
+            # print("-----------==========++++++++++==========-----------")
+
+            results = paper_RMA_DATT_eval(cfg=cfg, best_model=True, idx=idx)
+            current_results[0] = _scale[0]
+            current_results[1] = _seed
+            current_results[2:] = results
+
+            rma_datt_results[i, j, :] = current_results
+
+    return idx, phase_1_results, rma_datt_results
+
+
+for env_run in env_runs:
+    args = Args(env_id=env_run[0], run_name=env_run[1])
+    print("Running for env:", args.env_id, "run_name:", args.run_name)
+
+    cfg = Config(
+        env_id=args.env_id,
+        seed=args.seed,
+        eval=True,
+        run_name=args.run_name,
+        agent=args.agent,
+        scale=args.scale,
+    )
+
+    current_branch_name = (
+        subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        .decode("utf-8")
+        .strip()
+    )
+    print("Current branch name:", current_branch_name)
+    branch_name = "runs/" + cfg.experiment.grp_name + "/" + args.run_name
+
+    ### * EVAL CODE
+
+    sc = np.linspace(0.05, 0.16, 12)
+    print("Scale lengths:", sc)
+    sc_list = [[i, i] for i in sc]
+    num_sc_list = len(sc_list)
+    print("Scale lengths:", num_sc_list)
+
+    num_eval_trajs = 13  # 13 eval trajs #& HARDCODED for now
+    print("Number of eval trajs:", num_eval_trajs)
+
+    # create a list of seeds by incrementing cfg.seed by 1
+    num_seeds = 16
+    seeds = [cfg.seed + i for i in range(num_seeds)]
+    print("Seeds:", seeds)
+
+    all_phase_1_results = np.zeros((num_eval_trajs, num_seeds, num_sc_list, 8))
+    all_rma_datt_results = np.zeros((num_eval_trajs, num_seeds, num_sc_list, 8))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        results = executor.map(
+            trajectory_eval_idx,
+            range(num_eval_trajs),
+            repeat(num_seeds),
+            repeat(seeds),
+            repeat(num_sc_list),
+            repeat(sc_list),
+            repeat(cfg),
+        )
+
+        for idx, phase_1_results, rma_datt_results in results:
+            print("idx:", idx)
+            all_phase_1_results[idx] = phase_1_results
+            all_rma_datt_results[idx] = rma_datt_results
+
+    run_folder = "runs/" + cfg.grp_name + "/" + cfg.run_name + "/"
+    results_folder = run_folder + "results-icra/"
+
+    os.makedirs(results_folder, exist_ok=True)
+
+    print("Saving results to:", results_folder)
+    np.save(results_folder + "phase_1_results.npy", all_phase_1_results)
+    np.save(results_folder + "rma_datt_results.npy", all_rma_datt_results)
