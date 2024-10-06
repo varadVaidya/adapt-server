c112981ee684c728989a7063d8e9b7452a1e7003
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-10-06 18:53:05 +0530

    add script file

--- experiments/model_seed.sh
+++ experiments/model_seed.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Ensure the 'runs' directory exists
+mkdir -p runs/model_seed
+
 # Define the range of seeds
-seq 4551 4560 | xargs -P 2 -I {} python adapt_drones/train.py --env_id traj_v3 --seed {}
+seq 4551 4560 | xargs -P 25 -I {} bash -c 'python adapt_drones/eval.py --env_id traj_v3 --run_name true-durian-33 --seed {} > runs/model_seed/eval_seed_{}.txt 2>&1'
