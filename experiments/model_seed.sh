de2eee0e038d81d4dfccff4de6b5d1358da9e2a1
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-10-06 18:59:22 +0530

    server run

--- experiments/model_seed.sh
+++ experiments/model_seed.sh
@@ -4,4 +4,4 @@
 mkdir -p runs/model_seed
 
 # Define the range of seeds
-seq 4551 4560 | xargs -P 25 -I {} bash -c 'python adapt_drones/eval.py --env_id traj_v3 --run_name true-durian-33 --seed {} > runs/model_seed/eval_seed_{}.txt 2>&1'
+seq 4551 4580 | xargs -P 15 -I {} bash -c 'python adapt_drones/train.py --env_id traj_v3 --seed {} > runs/model_seed/eval_seed_{}.txt 2>&1'
