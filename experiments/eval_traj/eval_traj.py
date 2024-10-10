f7b7dd6c98bec4984b202691d37fc498889474b8
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-10-10 17:06:30 +0530

    cleanup experiments

--- experiments/eval_traj/eval_traj.py
+++ experiments/eval_traj/eval_traj.py
@@ -49,175 +49,187 @@ def evaluate_per_seed_per_scale(seed, scale, cfg, idx):
 
 
 if __name__ == "__main__":
-    env_run = ["traj_v3", "lemon-armadillo-68", True]
-    args = Args(env_id=env_run[0], run_name=env_run[1], wind_bool=env_run[2])
-
-    cfg = Config(
-        env_id=args.env_id,
-        seed=args.seed,
-        eval=True,
-        run_name=args.run_name,
-        agent=args.agent,
-        scale=args.scale,
-        wind_bool=args.wind_bool,
-    )
-
-    c = np.linspace(0.05, 0.22, 16)
-    seeds = np.arange(4551, 4551 + 16)
-    idx = np.arange(0, 13)
-    sc_list = [[i, i] for i in c]
-
-    print(sc_list)
-    print(seeds)
-    print(idx)
-
-    num_lengths = len(c)
-    num_seeds = len(seeds)
-    num_idx = len(idx)
-
-    map_iterable = [
-        (int(seed), scale, cfg, i) for seed in seeds for scale in sc_list for i in idx
+    env_runs = [
+        ["traj_v3", "earthy-snowball-77", True],
+        ["traj_v3", "fine-universe-76", True],
     ]
 
-    print(len(map_iterable))
+    for env_run in env_runs:
+        args = Args(env_id=env_run[0], run_name=env_run[1], wind_bool=env_run[2])
 
-    print(evaluate_per_seed_per_scale(*map_iterable[0]))
+        print(f"Running evaluation for {args.run_name} with wind: {args.wind_bool}")
 
-    # for i in tqdm.tqdm(range(len(map_iterable))):
-    #     print(evaluate_per_seed_per_scale(*map_iterable[i]))
-
-    traj_scale_eval = np.zeros((num_idx, num_seeds, num_lengths, 5))
-    traj_scale_eval[:, :, :, :] = np.nan
-
-    prefix = "wind" if env_run[2] else "no_wind"
-
-    with concurrent.futures.ProcessPoolExecutor(
-        max_workers=8,
-        mp_context=mp.get_context("spawn"),
-    ) as executor:
-        results = list(
-            tqdm.tqdm(
-                executor.map(evaluate_per_seed_per_scale, *zip(*map_iterable)),
-                total=len(map_iterable),
-            )
+        cfg = Config(
+            env_id=args.env_id,
+            seed=args.seed,
+            eval=True,
+            run_name=args.run_name,
+            agent=args.agent,
+            scale=args.scale,
+            wind_bool=args.wind_bool,
         )
 
-    for result in results:
-        _idx, _seed, _c, _mean_error, _rms_error = result
-        idx_idx = np.where(idx == _idx)[0][0]
-        seed_idx = np.where(seeds == _seed)[0][0]
-        c_idx = np.where(c == _c)[0][0]
+        c = np.linspace(0.05, 0.22, 16)
+        seeds = np.arange(4551, 4551 + 16)
+        idx = np.arange(0, 14)
+        sc_list = [[i, i] for i in c]
 
-        traj_scale_eval[idx_idx, seed_idx, c_idx] = [
-            _idx,
-            _seed,
-            _c,
-            _mean_error,
-            _rms_error,
+        print(sc_list)
+        print(seeds)
+        print(idx)
+
+        num_lengths = len(c)
+        num_seeds = len(seeds)
+        num_idx = len(idx)
+
+        map_iterable = [
+            (int(seed), scale, cfg, i)
+            for seed in seeds
+            for scale in sc_list
+            for i in idx
         ]
 
-    print(traj_scale_eval.shape)
+        print(len(map_iterable))
 
-    # check for nans
-    print(np.argwhere(np.isnan(traj_scale_eval)))
+        print(evaluate_per_seed_per_scale(*map_iterable[0]))
 
-    run_folder = "experiments/eval_traj/results-scale/"
+        # for i in tqdm.tqdm(range(len(map_iterable))):
+        #     print(evaluate_per_seed_per_scale(*map_iterable[i]))
 
-    os.makedirs(run_folder, exist_ok=True)
+        traj_scale_eval = np.zeros((num_idx, num_seeds, num_lengths, 5))
+        traj_scale_eval[:, :, :, :] = np.nan
 
-    np.save(run_folder + prefix + "traj_scale_eval.npy", traj_scale_eval)
+        prefix = "wind_" if env_run[2] else "no_wind_"
 
-    # remove the seeds which performed worst across all scales, in
-    # sense of mean error
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=12,
+            mp_context=mp.get_context("spawn"),
+        ) as executor:
+            results = list(
+                tqdm.tqdm(
+                    executor.map(evaluate_per_seed_per_scale, *zip(*map_iterable)),
+                    total=len(map_iterable),
+                )
+            )
 
-    idx_sort_eval = np.argsort(traj_scale_eval[:, :, :, 3], axis=2)
-    sorted_traj_eval = traj_scale_eval[:, idx_sort_eval[0], :, :]
+        for result in results:
+            _idx, _seed, _c, _mean_error, _rms_error = result
+            idx_idx = np.where(idx == _idx)[0][0]
+            seed_idx = np.where(seeds == _seed)[0][0]
+            c_idx = np.where(c == _c)[0][0]
 
-    # remove top 3 and bottom 3 seeds
-    # traj_scale_eval = sorted_traj_eval[:, 3:-3, :, :]
+            traj_scale_eval[idx_idx, seed_idx, c_idx] = [
+                _idx,
+                _seed,
+                _c,
+                _mean_error,
+                _rms_error,
+            ]
 
-    # compile the data into a csv file contain errors and std dev
-    data_compile = np.zeros((traj_scale_eval.shape[0] * traj_scale_eval.shape[2], 6))
+        print(traj_scale_eval.shape)
 
-    # ^ 0: idx, 1: c,
-    # ^ 3:  avg mean error, 4: std dev mean error,
-    # ^ 4: avg rms error, 5: std dev rms error
+        # check for nans
+        print(np.argwhere(np.isnan(traj_scale_eval)))
 
-    data_compile[:, 0] = np.repeat(
-        np.arange(traj_scale_eval.shape[0]), traj_scale_eval.shape[2]
-    )  # tile idx
+        run_folder = f"experiments/eval_traj/results-scale/{env_run[1]}/"
 
-    data_compile[:, 1] = np.tile(
-        traj_scale_eval[0, 0, :, 2], traj_scale_eval.shape[0]
-    )  # tile c
+        os.makedirs(run_folder, exist_ok=True)
 
-    data_compile[:, 2] = np.mean(traj_scale_eval[:, :, :, 3], axis=1).reshape(
-        traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
-    )  # avg mean error
+        np.save(run_folder + prefix + "traj_scale_eval.npy", traj_scale_eval)
 
-    data_compile[:, 3] = np.std(traj_scale_eval[:, :, :, 3], axis=1).reshape(
-        traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
-    )  # std dev mean error
+        # remove the seeds which performed worst across all scales, in
+        # sense of mean error
 
-    data_compile[:, 4] = np.mean(traj_scale_eval[:, :, :, 4], axis=1).reshape(
-        traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
-    )  # avg rms error
+        idx_sort_eval = np.argsort(traj_scale_eval[:, :, :, 3], axis=2)
+        sorted_traj_eval = traj_scale_eval[:, idx_sort_eval[0], :, :]
 
-    data_compile[:, 5] = np.std(traj_scale_eval[:, :, :, 4], axis=1).reshape(
-        traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
-    )  # std dev rms error
+        # remove top 3 and bottom 3 seeds
+        # traj_scale_eval = sorted_traj_eval[:, 3:-3, :, :]
 
-    print(data_compile.shape)
+        # compile the data into a csv file contain errors and std dev
+        data_compile = np.zeros(
+            (traj_scale_eval.shape[0] * traj_scale_eval.shape[2], 6)
+        )
 
-    print("Saving data_compile to: ", run_folder + prefix + "mpc_traj_excel.csv")
+        # ^ 0: idx, 1: c,
+        # ^ 3:  avg mean error, 4: std dev mean error,
+        # ^ 4: avg rms error, 5: std dev rms error
 
-    header = [
-        "idx",
-        "c",
-        "mean_error",
-        "std_dev_mean_error",
-        "rms_error",
-        "std_dev_rms_error",
-    ]
+        data_compile[:, 0] = np.repeat(
+            np.arange(traj_scale_eval.shape[0]), traj_scale_eval.shape[2]
+        )  # tile idx
 
-    np.savetxt(
-        run_folder + prefix + "mpc_traj_excel.csv",
-        data_compile,
-        delimiter=",",
-        header=",".join(header),
-        comments="",
-    )
+        data_compile[:, 1] = np.tile(
+            traj_scale_eval[0, 0, :, 2], traj_scale_eval.shape[0]
+        )  # tile c
 
-    def get_error_info(df, idx_value, c_value):
-        # Filter the row based on idx and c values
-        row = df.loc[(df["idx"] == idx_value) & (df["c"] == c_value)]
+        data_compile[:, 2] = np.mean(traj_scale_eval[:, :, :, 3], axis=1).reshape(
+            traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
+        )  # avg mean error
 
-        # If the row exists, round and concatenate mean_error and std_dev_mean_error
-        if not row.empty:
-            mean_error = round(row["mean_error"].values[0], 4)
-            std_dev_mean_error = round(row["std_dev_mean_error"].values[0], 4)
-            return f"{mean_error} \pm {std_dev_mean_error}"
-        else:
-            return "No match found"
+        data_compile[:, 3] = np.std(traj_scale_eval[:, :, :, 3], axis=1).reshape(
+            traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
+        )  # std dev mean error
 
-    df = pd.read_csv(run_folder + prefix + "mpc_traj_excel.csv")
+        data_compile[:, 4] = np.mean(traj_scale_eval[:, :, :, 4], axis=1).reshape(
+            traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
+        )  # avg rms error
 
-    tex_table = []
-    for idx_value in np.unique(df["idx"]):
-        idx_row = []
-        for c_value in np.unique(df["c"]):
-            idx_row.append(get_error_info(df, idx_value, c_value))
-        tex_table.append(idx_row)
+        data_compile[:, 5] = np.std(traj_scale_eval[:, :, :, 4], axis=1).reshape(
+            traj_scale_eval.shape[0] * traj_scale_eval.shape[2]
+        )  # std dev rms error
 
-    # convert tex_table to pandas dataframe
-    tex_table = np.array(tex_table)
-    tex_table = pd.DataFrame(
-        tex_table,
-        columns=np.round(
-            np.unique(df["c"]),
-            3,
-        ),
-        index=np.unique(df["idx"]),
-    )
-    # save the table to a csv file
-    tex_table.to_csv(run_folder + prefix + "mpc_table.csv")
+        print(data_compile.shape)
+
+        print("Saving data_compile to: ", run_folder + prefix + "mpc_traj_excel.csv")
+
+        header = [
+            "idx",
+            "c",
+            "mean_error",
+            "std_dev_mean_error",
+            "rms_error",
+            "std_dev_rms_error",
+        ]
+
+        np.savetxt(
+            run_folder + prefix + "traj_excel.csv",
+            data_compile,
+            delimiter=",",
+            header=",".join(header),
+            comments="",
+        )
+
+        def get_error_info(df, idx_value, c_value):
+            # Filter the row based on idx and c values
+            row = df.loc[(df["idx"] == idx_value) & (df["c"] == c_value)]
+
+            # If the row exists, round and concatenate mean_error and std_dev_mean_error
+            if not row.empty:
+                mean_error = round(row["mean_error"].values[0], 4)
+                std_dev_mean_error = round(row["std_dev_mean_error"].values[0], 4)
+                return f"{mean_error} \pm {std_dev_mean_error}"
+            else:
+                return "No match found"
+
+        df = pd.read_csv(run_folder + prefix + "traj_excel.csv")
+
+        tex_table = []
+        for idx_value in np.unique(df["idx"]):
+            idx_row = []
+            for c_value in np.unique(df["c"]):
+                idx_row.append(get_error_info(df, idx_value, c_value))
+            tex_table.append(idx_row)
+
+        # convert tex_table to pandas dataframe
+        tex_table = np.array(tex_table)
+        tex_table = pd.DataFrame(
+            tex_table,
+            columns=np.round(
+                np.unique(df["c"]),
+                3,
+            ),
+            index=np.unique(df["idx"]),
+        )
+        # save the table to a csv file
+        tex_table.to_csv(run_folder + prefix + "traj_eval_table.csv")
--- experiments/xadapt/xadapt_range.py
+++ experiments/xadapt/xadapt_range.py
@@ -75,179 +75,194 @@ def simulate_traj_rma(idx, seed, c):
 
 
 if __name__ == "__main__":
-    c = np.linspace(0, 1, 15)
-    seeds = np.arange(4551, 4551 + 16)
-    idx = np.arange(0, 13)
-
-    print(c)
-    print(seeds)
-    print(idx)
-
-    num_lengths = len(c)
-    num_seeds = len(seeds)
-    num_idx = len(idx)
-
-    traj_rma_eval = np.zeros((num_idx, num_seeds, num_lengths, 5))
-    traj_rma_eval[:, :, :, :] = np.nan
-
-    map_iterable = [
-        (int(i), int(seed), float(_c)) for i in idx for seed in seeds for _c in c
+    env_runs = [
+        ["traj_v3", "earthy-snowball-77", True],
+        ["traj_v3", "fine-universe-76", True],
     ]
-    print(len(map_iterable))
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+    for env_run in env_runs:
+        args = Args(env_id=env_run[0], run_name=env_run[1], wind_bool=env_run[2])
 
-        results = list(
-            tqdm.tqdm(
-                executor.map(
-                    simulate_traj_rma,
-                    *zip(*map_iterable),
-                ),
-                total=len(map_iterable),
+        print(f"Running evaluation for {args.run_name} with wind: {args.wind_bool}")
+
+        cfg.env_id = args.env_id
+        cfg.run_name = args.run_name
+        cfg.wind_bool = args.wind_bool
+
+        c = np.linspace(0, 1, 16)
+        seeds = np.arange(4551, 4551 + 16)
+        idx = np.arange(0, 14)
+
+        print(c)
+        print(seeds)
+        print(idx)
+
+        num_lengths = len(c)
+        num_seeds = len(seeds)
+        num_idx = len(idx)
+
+        traj_rma_eval = np.zeros((num_idx, num_seeds, num_lengths, 5))
+        traj_rma_eval[:, :, :, :] = np.nan
+
+        map_iterable = [
+            (int(i), int(seed), float(_c)) for i in idx for seed in seeds for _c in c
+        ]
+        print(len(map_iterable))
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=12) as executor:
+
+            results = list(
+                tqdm.tqdm(
+                    executor.map(
+                        simulate_traj_rma,
+                        *zip(*map_iterable),
+                    ),
+                    total=len(map_iterable),
+                )
             )
+
+        for result in results:
+            _idx, _seed, _c, _mean_error, _rms_error = result
+            idx_idx = np.where(idx == _idx)[0][0]
+            seed_idx = np.where(seeds == _seed)[0][0]
+            c_idx = np.where(c == _c)[0][0]
+
+            traj_rma_eval[idx_idx, seed_idx, c_idx, :] = [
+                _idx,
+                _seed,
+                _c,
+                _mean_error,
+                _rms_error,
+            ]
+
+        print(traj_rma_eval.shape)
+
+        # # check for nans
+        # print(np.argwhere(np.isnan(traj_rma_eval)))
+        run_folder = f"experiments/xadapt/results-xadapt/{env_run[1]}/"
+        prefix = "wind_" if cfg.wind_bool else "no_wind_"
+        results_folder = run_folder
+
+        os.makedirs(results_folder, exist_ok=True)
+
+        np.save(results_folder + prefix + "xadapt_traj_rma.npy", traj_rma_eval)
+
+        # traj_rma_eval = np.load(results_folder + "xadapt_traj_rma.npy")
+        ## remove the seeds which performed worst across all scales, in
+        # sense of mean error
+
+        idx_sort_eval = np.argsort(np.mean(traj_rma_eval[:, :, :, 3], axis=2))
+        sorted_traj_eval = traj_rma_eval[:, idx_sort_eval[0], :, :]
+
+        # remove the top 3 and bottom 3 seeds
+        traj_rma_eval = sorted_traj_eval[:, 3:-3, :, :]
+
+        # compile the data into a csv file contain errors and std dev
+        data_compile = np.zeros((traj_rma_eval.shape[0] * traj_rma_eval.shape[2], 7))
+
+        # ^ 0: idx, 1: c,
+        # ^ 3:  avg mean error, 4: std dev mean error,
+        # ^ 4: avg rms error, 5: std dev rms error
+        # ^ 6: crashed: num of seeds crashed
+
+        data_compile[:, 0] = np.repeat(
+            np.arange(traj_rma_eval.shape[0]), traj_rma_eval.shape[2]
+        )  # tile idx
+        data_compile[:, 1] = np.tile(
+            traj_rma_eval[0, 0, :, 2], traj_rma_eval.shape[0]
+        )  # tile c
+
+        data_compile[:, 2] = np.mean(
+            traj_rma_eval[:, :, :, 3],
+            axis=1,
+            where=~np.isinf(traj_rma_eval[:, :, :, 3]),
+        ).reshape(
+            traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
+        )  # avg mean error
+
+        data_compile[:, 3] = np.std(
+            traj_rma_eval[:, :, :, 3],
+            axis=1,
+            where=~np.isinf(traj_rma_eval[:, :, :, 3]),
+        ).reshape(
+            traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
+        )  # std dev mean error
+
+        data_compile[:, 4] = np.mean(
+            traj_rma_eval[:, :, :, 4],
+            axis=1,
+            where=~np.isinf(traj_rma_eval[:, :, :, 4]),
+        ).reshape(
+            traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
+        )  # avg rms error
+
+        data_compile[:, 5] = np.std(
+            traj_rma_eval[:, :, :, 4],
+            axis=1,
+            where=~np.isinf(traj_rma_eval[:, :, :, 4]),
+        ).reshape(
+            traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
+        )  # std dev rms error
+
+        data_compile[:, 6] = np.sum(
+            traj_rma_eval[:, :, :, 3] == np.inf,
+            axis=1,
+        ).reshape(
+            traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
+        )  # crashed count
+
+        print(data_compile.shape)
+        print("Saving results to:")
+        header = [
+            "idx",
+            "c",
+            "mean_error",
+            "std_dev_mean_error",
+            "rms_error",
+            "std_dev_rms_error",
+            "crashed",
+        ]
+        np.savetxt(
+            results_folder + prefix + "xadapt_traj_excel.csv",
+            data_compile,
+            delimiter=",",
+            header=",".join(header),
+            comments="",
         )
 
-    for result in results:
-        _idx, _seed, _c, _mean_error, _rms_error = result
-        idx_idx = np.where(idx == _idx)[0][0]
-        seed_idx = np.where(seeds == _seed)[0][0]
-        c_idx = np.where(c == _c)[0][0]
+        import pandas as pd
 
-        traj_rma_eval[idx_idx, seed_idx, c_idx, :] = [
-            _idx,
-            _seed,
-            _c,
-            _mean_error,
-            _rms_error,
-        ]
+        def get_error_info(df, idx_value, c_value):
+            # Filter the row based on idx and c values
+            row = df.loc[(df["idx"] == idx_value) & (df["c"] == c_value)]
 
-    print(traj_rma_eval.shape)
+            # If the row exists, round and concatenate mean_error and std_dev_mean_error
+            if not row.empty:
+                mean_error = round(row["mean_error"].values[0], 3)
+                std_dev_mean_error = round(row["std_dev_mean_error"].values[0], 3)
+                return f"{mean_error} \pm {std_dev_mean_error}"
+            else:
+                return "No match found"
 
-    # # check for nans
-    # print(np.argwhere(np.isnan(traj_rma_eval)))
-    run_folder = "experiments/xadapt/results-xadapt/"
-    prefix = "wind_" if cfg.wind_bool else "no_wind_"
-    results_folder = run_folder
+        df = pd.read_csv(results_folder + prefix + "xadapt_traj_excel.csv")
 
-    os.makedirs(results_folder, exist_ok=True)
+        tex_table = []
+        for idx_value in np.unique(df["idx"]):
+            idx_row = []
+            for c_value in np.unique(df["c"]):
+                idx_row.append(get_error_info(df, idx_value, c_value))
+            tex_table.append(idx_row)
 
-    np.save(results_folder + prefix + "xadapt_traj_rma.npy", traj_rma_eval)
-
-    # traj_rma_eval = np.load(results_folder + "xadapt_traj_rma.npy")
-    ## remove the seeds which performed worst across all scales, in
-    # sense of mean error
-
-    idx_sort_eval = np.argsort(np.mean(traj_rma_eval[:, :, :, 3], axis=2))
-    sorted_traj_eval = traj_rma_eval[:, idx_sort_eval[0], :, :]
-
-    # remove the top 3 and bottom 3 seeds
-    traj_rma_eval = sorted_traj_eval[:, 3:-3, :, :]
-
-    # compile the data into a csv file contain errors and std dev
-    data_compile = np.zeros((traj_rma_eval.shape[0] * traj_rma_eval.shape[2], 7))
-
-    # ^ 0: idx, 1: c,
-    # ^ 3:  avg mean error, 4: std dev mean error,
-    # ^ 4: avg rms error, 5: std dev rms error
-    # ^ 6: crashed: num of seeds crashed
-
-    data_compile[:, 0] = np.repeat(
-        np.arange(traj_rma_eval.shape[0]), traj_rma_eval.shape[2]
-    )  # tile idx
-    data_compile[:, 1] = np.tile(
-        traj_rma_eval[0, 0, :, 2], traj_rma_eval.shape[0]
-    )  # tile c
-
-    data_compile[:, 2] = np.mean(
-        traj_rma_eval[:, :, :, 3],
-        axis=1,
-        where=~np.isinf(traj_rma_eval[:, :, :, 3]),
-    ).reshape(
-        traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
-    )  # avg mean error
-
-    data_compile[:, 3] = np.std(
-        traj_rma_eval[:, :, :, 3],
-        axis=1,
-        where=~np.isinf(traj_rma_eval[:, :, :, 3]),
-    ).reshape(
-        traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
-    )  # std dev mean error
-
-    data_compile[:, 4] = np.mean(
-        traj_rma_eval[:, :, :, 4],
-        axis=1,
-        where=~np.isinf(traj_rma_eval[:, :, :, 4]),
-    ).reshape(
-        traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
-    )  # avg rms error
-
-    data_compile[:, 5] = np.std(
-        traj_rma_eval[:, :, :, 4],
-        axis=1,
-        where=~np.isinf(traj_rma_eval[:, :, :, 4]),
-    ).reshape(
-        traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
-    )  # std dev rms error
-
-    data_compile[:, 6] = np.sum(
-        traj_rma_eval[:, :, :, 3] == np.inf,
-        axis=1,
-    ).reshape(
-        traj_rma_eval.shape[0] * traj_rma_eval.shape[2]
-    )  # crashed count
-
-    print(data_compile.shape)
-    print("Saving results to:")
-    header = [
-        "idx",
-        "c",
-        "mean_error",
-        "std_dev_mean_error",
-        "rms_error",
-        "std_dev_rms_error",
-        "crashed",
-    ]
-    np.savetxt(
-        results_folder + prefix + "xadapt_traj_excel.csv",
-        data_compile,
-        delimiter=",",
-        header=",".join(header),
-        comments="",
-    )
-
-    import pandas as pd
-
-    def get_error_info(df, idx_value, c_value):
-        # Filter the row based on idx and c values
-        row = df.loc[(df["idx"] == idx_value) & (df["c"] == c_value)]
-
-        # If the row exists, round and concatenate mean_error and std_dev_mean_error
-        if not row.empty:
-            mean_error = round(row["mean_error"].values[0], 3)
-            std_dev_mean_error = round(row["std_dev_mean_error"].values[0], 3)
-            return f"{mean_error} \pm {std_dev_mean_error}"
-        else:
-            return "No match found"
-
-    df = pd.read_csv(results_folder + prefix + "xadapt_traj_excel.csv")
-
-    tex_table = []
-    for idx_value in np.unique(df["idx"]):
-        idx_row = []
-        for c_value in np.unique(df["c"]):
-            idx_row.append(get_error_info(df, idx_value, c_value))
-        tex_table.append(idx_row)
-
-    # convert tex_table to pandas dataframe
-    # print(Dynamics(seed=0, c=np.unique(df["c"]), do_random=False).length_scale())
-    tex_table = np.array(tex_table)
-    tex_table = pd.DataFrame(
-        tex_table,
-        columns=np.round(
-            Dynamics(seed=0, c=np.unique(df["c"]), do_random=False).length_scale(), 3
-        ),
-        index=np.unique(df["idx"]),
-    )
-    # save the table to a csv file
-    tex_table.to_csv(results_folder + prefix + "xadapt_table.csv")
+        # convert tex_table to pandas dataframe
+        # print(Dynamics(seed=0, c=np.unique(df["c"]), do_random=False).length_scale())
+        tex_table = np.array(tex_table)
+        tex_table = pd.DataFrame(
+            tex_table,
+            columns=np.round(
+                Dynamics(seed=0, c=np.unique(df["c"]), do_random=False).length_scale(),
+                3,
+            ),
+            index=np.unique(df["idx"]),
+        )
+        # save the table to a csv file
+        tex_table.to_csv(results_folder + prefix + "xadapt_table.csv")
