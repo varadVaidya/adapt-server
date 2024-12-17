550d54f8b10e1dad1d0d7f81fb9afc942f9be1d4
Varad Vaidya <vaidyavarad2001@gmail.com>
2024-12-17 13:58:48 +0530

    update angular velocity reward

--- adapt_drones/envs/TrajAviaryv3.py
+++ adapt_drones/envs/TrajAviaryv3.py
@@ -263,7 +263,7 @@ class TrajAviaryv3(BaseAviary):
         )
 
         # weights = np.array([0.50, 0.15, 0.15, 0.15, 0.05])
-        weights = np.array([0.55, 0.15, 0.2, 0.05, 0.05])
+        weights = np.array([0.50, 0.15, 0.2, 0.1, 0.05])
         weights = weights / np.sum(weights)
         reward_vector = np.array(
             [
